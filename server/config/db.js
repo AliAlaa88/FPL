134edf0788c92a87296dd7e23e0ced0b3330189c
AliAlaa88 <alieldinalaa04@gmail.com>
2026-02-02 00:24:00 +0200

    Remove dotenv import, add pg-native to externals

--- server/config/db.js
+++ server/config/db.js
@@ -1,11 +1,4 @@
 import { Sequelize } from "sequelize";
-import dotenv from "dotenv";
-
-// Load .env only for local development
-// In production (Netlify), env vars are injected directly
-if (process.env.NODE_ENV !== "production") {
-  dotenv.config();
-}
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: "postgres",
